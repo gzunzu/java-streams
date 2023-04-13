2fb6781135844d2dcd62c533e0289922f46ddf7b
Germán Zunzunegui <gerzunzu@gmail.com>
2023-04-13 23:19:58 +0200

    Fix: Private no args constructor for utils classes.

--- src/main/java/org/example/utils/MappingUtils.java
+++ src/main/java/org/example/utils/MappingUtils.java
@@ -1,10 +1,13 @@
 package org.example.utils;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Map;
 import java.util.function.Function;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MappingUtils {
 
     public static <T, E> Function<Map.Entry<T, E>, Pair<T, E>> mapEntryToPair() {
